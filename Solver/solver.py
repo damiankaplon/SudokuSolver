40d21f2c5b31b8677ce7333fd9276c9d047c888c
Damian Kaplon <kaplon.damian99@gmail.com>
2021-10-10 14:37:18 +0200

    Finish solver

--- Solver/solver.py
+++ Solver/solver.py
@@ -6,35 +6,45 @@ __email__ = 'kaplon.damian99@gmail.com'
 __copyright___ = "Copyright (c) 2021 Damian Kaplon"
 
 
-def find_empty(board: list) -> list:
+def find_empty(board: list) -> list or bool:
     """returns position of next empty place in board. If board has no empty place returns list which contains False"""
-    for i in range(len(board) - 1):
-        for j in range(len(board[i]) - 1):
+    for i in range(len(board)):
+        for j in range(len(board[i])):
             if board[i][j] == 0:
                 return [i, j]
-    return [False]
+    return False
 
 
-def solve(board: list) -> list:
-    pass
+def solve(board: list) -> bool and list:
+    empty = find_empty(board)
+    if not empty:
+        return True, board
+
+    row, column = empty
+    for i in range(1, 10):
+        if check_value(board, row, column, i):
+            board[row][column] = i
+            if solve(board):
+                return True, board
+
+            board[row][column] = 0
+    return False
 
 
 def check_value(board: list, row: int, column: int, value: int) -> bool:
     if value in board[row]:
         return False
 
-    for i in range(len(board) - 1):
+    for i in range(len(board)):
         if value == board[i][column]:
             return False
 
     x = row // 3
     y = column // 3
 
-    for i in range(0, 2):
-        for j in range(0, 2):
+    for i in range(0, 3):
+        for j in range(0, 3):
             if value == board[i + 3 * x][j + 3 * y]:
                 return False
 
     return True
-
-
--- Tests/solver_test.py
+++ Tests/solver_test.py
@@ -21,21 +21,34 @@ class SolverTest(TestCase):
                              [1, 0, 0, 0, 9, 0, 0, 0, 3],
                              [0, 0, 9, 8, 0, 0, 0, 6, 0]]
 
+        self.correct_solution = [[8, 2, 7, 1, 5, 4, 3, 9, 6],
+                                 [9, 6, 5, 3, 2, 7, 1, 4, 8],
+                                 [3, 4, 1, 6, 8, 9, 7, 5, 2],
+                                 [5, 9, 3, 4, 6, 8, 2, 7, 1],
+                                 [4, 7, 2, 5, 1, 3, 6, 8, 9],
+                                 [6, 1, 8, 9, 7, 2, 4, 3, 5],
+                                 [7, 8, 6, 2, 3, 5, 9, 1, 4],
+                                 [1, 5, 4, 7, 9, 6, 8, 2, 3],
+                                 [2, 3, 9, 8, 4, 1, 5, 6, 7]]
+
     def test_solve(self) -> None:
-        correct_solution = [[8, 2, 7, 1, 5, 4, 3, 9, 6],
-                            [9, 6, 5, 3, 2, 7, 1, 4, 8],
-                            [3, 4, 1, 6, 8, 9, 7, 5, 2],
-                            [5, 9, 3, 4, 6, 8, 2, 7, 1],
-                            [4, 7, 2, 5, 1, 3, 6, 8, 9],
-                            [6, 1, 8, 9, 7, 2, 4, 3, 5],
-                            [7, 8, 6, 2, 3, 5, 9, 1, 4],
-                            [1, 5, 4, 7, 9, 6, 8, 2, 3],
-                            [2, 3, 9, 8, 4, 1, 5, 6, 7]]
         solver_solution: list = solve(self.sudoku_board)
-        self.assertEqual(correct_solution, solver_solution)
+        self.assertEqual(self.correct_solution, solver_solution)
+
+    def test_find_empty1(self) -> None:
+        self.assertEqual(False, find_empty(self.correct_solution)[0])
+
+    def test_find_empty2(self) -> None:
+        self.assertEqual([0, 0], find_empty(self.sudoku_board))
+
+    def test_find_empty3(self) -> None:
+        some_sudoku_board = [[1, 2, 3, 4],
+                             [1, 2, 3, 5],
+                             [6, 9, 8, 0]]
+        self.assertEqual([2, 3], find_empty(some_sudoku_board))
 
     def test_check_value(self) -> None:
-        self.sudoku_board = [[0, 2, 0, 0, 0, 4, 3, 0, 0],
+        some_sudoku_board = [[0, 2, 0, 0, 0, 4, 3, 0, 0],
                              [9, 2, 0, 0, 2, 0, 0, 0, 8],
                              [0, 0, 0, 6, 0, 9, 0, 5, 0],
                              [0, 0, 0, 0, 0, 0, 0, 0, 1],
@@ -44,4 +57,16 @@ class SolverTest(TestCase):
                              [0, 8, 0, 2, 0, 5, 0, 0, 0],
                              [1, 0, 0, 0, 9, 0, 0, 0, 3],
                              [0, 0, 9, 8, 0, 0, 0, 6, 0]]
-        self.assertEqual(False, check_value(self.sudoku_board, 4, 4, 5))
+        self.assertEqual(False, check_value(some_sudoku_board, 4, 4, 5))
+
+    def test_check_value2(self) -> None:
+        some_sudoku_board = [[5, 2, 1, 7, 0, 4, 3, 0, 0],
+                             [9, 0, 0, 0, 2, 0, 0, 0, 8],
+                             [0, 0, 0, 6, 0, 9, 0, 5, 0],
+                             [0, 0, 0, 0, 0, 0, 0, 0, 1],
+                             [0, 7, 2, 5, 0, 3, 6, 8, 0],
+                             [6, 0, 0, 0, 0, 0, 0, 0, 0],
+                             [0, 8, 0, 2, 0, 5, 0, 0, 0],
+                             [1, 0, 0, 0, 9, 0, 0, 0, 3],
+                             [0, 0, 9, 8, 0, 0, 0, 6, 0]]
+        self.assertEqual(False, check_value(some_sudoku_board, 0, 4, 6))
