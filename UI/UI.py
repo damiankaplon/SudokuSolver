3f88e9b62142271e0b2044954c3573a86cb9250a
Damian <kaplon.damian99@gmail.com>
2021-10-14 11:49:06 +0200

    Add menu buttons methods

--- UI/UI.py
+++ UI/UI.py
@@ -2,8 +2,10 @@
 import string
 from tkinter import Tk, Button
 
+from Solver.solver import solve
 
-def onclick_button(event):
+
+def increment_button(event):
     state: string = event.widget['state']
     if state == "disabled":
         return
@@ -16,30 +18,42 @@ def onclick_button(event):
     event.widget['text'] = i
     return
 
+def solve_button(event, board: list):
+    
+    return solve(board)
+
 
 class UI(Tk):
     def __init__(self, board: list = None):
         super().__init__()
         self.board = board
         self.title("Sudoku!")
+        self.rowconfigure(9)
         self.buttons: list = list()
-        self.__create_buttons()
+        self.__create_main_board_buttons()
 
-    def __create_buttons(self) -> None:
-        for i in range(0, 6):
-            for j in range(0, 6):
+    def __create_main_board_buttons(self) -> None:
+        for i in range(0, 9):
+            for j in range(0, 9):
                 if self.board is not None:
                     text = self.board[i][j]
                 else:
                     text = 0
 
                 if text != 0:
-                    self.__create_button(text, i, j, state="disabled")
+                    self.__create_main_board_button(text, i, j, state="disabled")
                 else:
-                    self.__create_button(text, i, j)
+                    self.__create_main_board_button(text, i, j)
+        self.__create_menu_buttons()
 
-    def __create_button(self, text: string, row: int, column: int, state: string = "normal") -> None:
-        button = Button(self, height=5, width=5, text=text, state=state)
-        button.bind("<Button-1>", onclick_button)
+    def __create_menu_buttons(self) -> None:
+        button = Button(self, height=3, width=5, text="Solve", bd=4)
+        button.bind("<Button-1>", solve_button(self.board))
+        button.grid(column=7, row=10, columnspan=2)
+        self.buttons.append(button)
+
+    def __create_main_board_button(self, text: string, row: int, column: int, state: string = "normal") -> None:
+        button = Button(self, height=3, width=5, text=text, state=state, bd=4)
+        button.bind("<Button-1>", increment_button)
         button.grid(column=column, row=row)
         self.buttons.append(button)
--- main.py
+++ main.py
@@ -8,5 +8,14 @@ __copyright___ = "Copyright (c) 2021 Damian Kaplon"
 from UI.UI import UI
 
 if __name__ == "__main__":
-    main_window = UI()
+    sudoku_board = [[0, 2, 0, 0, 0, 4, 3, 0, 0],
+                    [9, 0, 0, 0, 2, 0, 0, 0, 8],
+                    [0, 0, 0, 6, 0, 9, 0, 5, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 1],
+                    [0, 7, 2, 5, 0, 3, 6, 8, 0],
+                    [6, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 8, 0, 2, 0, 5, 0, 0, 0],
+                    [1, 0, 0, 0, 9, 0, 0, 0, 3],
+                    [0, 0, 9, 8, 0, 0, 0, 6, 0]]
+    main_window = UI(sudoku_board)
     main_window.mainloop()
